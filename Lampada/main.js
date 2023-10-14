6784ea13b90660dd28743033a92e54f0d3ded866
Matheus Pacheco Cruz <pachecx35@gmail.com>
2023-10-14 08:36:59 -0300

    up

--- Lampada/main.js
+++ Lampada/main.js
@@ -3,10 +3,37 @@ const imgOn = document.querySelector('.on');
 const imgBroken = document.querySelector('.broken');
 
 const btnOn = document.querySelector('.ligar');
-const btnBroken = document.querySelector('.desligar'); 
+const btnOff = document.querySelector('.desligar'); 
+const Imgs = document.querySelector('.imgs')
 
 
 btnOn.addEventListener('click', function(){
     imgOff.classList.add('hide')
     imgOn.classList.remove('hide')
+    imgBroken.classList.add('hide')
+    Imgs.classList.remove('hide')
+})
+
+btnOff.addEventListener('click', function(){
+    imgOff.classList.remove('hide')
+    imgOn.classList.add('hide')
+    imgBroken.classList.add('hide')
+    Imgs.classList.remove('hide')
+})
+
+Imgs.addEventListener('mouseover', function(){
+    imgOff.classList.add('hide')
+    imgOn.classList.remove('hide')
+
+})
+
+Imgs.addEventListener('mouseout', function(){
+    imgOff.classList.remove('hide')
+    imgOn.classList.add('hide')
+
+})
+
+imgOn.addEventListener('dblclick', function(){
+    imgBroken.classList.remove('hide')
+    Imgs.classList.add('hide')
 })
